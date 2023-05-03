928bd100a6fb5494e35a55abe3b830887a1cb46f
Mingyang_Li <li2856652788@gmail.com>
2023-05-03 20:20:08 +0800

    5.3

--- src/network/promotion/index.js
+++ src/network/promotion/index.js
@@ -3,5 +3,5 @@ const testAPI = require("@/apis")
 
 
 export const getAllPromotion = config => request._get(testAPI.PROMOTION_ALL,config);
-export const Approval = config => request._post(testAPI.PROMOTION_APPROVAL, config);
+export const Approval = config => request._get(testAPI.PROMOTION_APPROVAL, config);
 export const createPromotion = config =>request._post(testAPI.PROMOTION_CREATE,config);
